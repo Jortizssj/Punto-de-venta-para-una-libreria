29574c5ff180bb97baf718b666e4025303b43329
Jortizssj <167387428+Jortizssj@users.noreply.github.com>
2025-11-26 13:43:46 -0600

    Estructura inicial en capas y CRUD de Productos

---
+++ Proyecto_libreria/ProductoDAO.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_libreria
+{
+    internal class ProductoDAO
+    {
+        // TODO: Reemplazar con tu cadena de conexión real
+        private string connectionString = "Data Source=.;Initial Catalog=VENTAS;Integrated Security=True";
+
+        // Método para obtener todos los productos
+        public DataTable ObtenerTodosProductos()
+        {
+            DataTable dt = new DataTable();
+            // Uso de TRY-CATCH-FINALLY para control de errores
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    // Consulta simple para mostrar en la cuadrícula
+                    string query = "SELECT CLAVE as ISBN, NOMBRE, DESCRIPCION, PRECIO, STOCK FROM PRODUCTOS ORDER BY NOMBRE";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    connection.Open();
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    adapter.Fill(dt);
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Error de BD al obtener productos: " + ex.Message);
+            }
+            finally
+            {
+            }
+
+            return dt;
+        }
+
+        // Método para registrar un nuevo producto (INSERT)
+        public bool AgregarProducto(clsProducto producto)
+        {
+            bool exito = false;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    string query = @"
+                        INSERT INTO PRODUCTOS (CLAVE, NOMBRE, DESCRIPCION, PRECIO, STOCK) 
+                        VALUES (@Clave, @Nombre, @Descripcion, @Precio, @Stock)";
+
+                    SqlCommand command = new SqlCommand(query, connection);
+
+                    // Asignación de parámetros
+                    command.Parameters.AddWithValue("@Clave", producto.ClaveISBN);
+                    command.Parameters.AddWithValue("@Nombre", producto.Nombre);
+                    command.Parameters.AddWithValue("@Descripcion", producto.Descripcion);
+                    command.Parameters.AddWithValue("@Precio", producto.Precio);
+                    command.Parameters.AddWithValue("@Stock", producto.Stock);
+
+                    connection.Open();
+                    int filasAfectadas = command.ExecuteNonQuery();
+                    if (filasAfectadas > 0)
+                    {
+                        exito = true;
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Error de BD al agregar producto: " + ex.Message);
+                exito = false;
+            }
+            return exito;
+        }
+
+    }
+}
+}
---
+++ Proyecto_libreria/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("Proyecto_libreria")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Proyecto_libreria")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("2c0287c9-c502-438a-ac91-27880e7c12cf")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
